2ad41675e21754fffddf413767e107c51c75df47
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-08-13 12:47:28 +0200

    deleted styled.js from container and added styles do Container component

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,9 +1,17 @@
-import { Wrapper } from "./styled";
+import styled from "styled-components";
 
-const Container = ({children}) => (
-    <Wrapper>
-        {children}
-    </Wrapper>
-);
+export const Container = styled.main`
+  max-width: 800px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 768px) {
+
+  .button {
+    margin-bottom: 10px;
+  }
+}
+`;
 
 export default Container;
