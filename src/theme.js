0d9d5fc570d216db146640276cb043b3c938111c
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-08-13 12:46:47 +0200

    added theme

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+    colors: {
+      teal: "hsl(180, 100%, 25%)",
+      white: "hsl(0, 0%, 100%)",
+      dirtyWhite: "hsl(0, 0%, 87%)",
+      green: "hsl(120, 93%, 35%)",
+      lightGreen: "hsl(120, 93%, 40%)",
+      lightBlack: "hsl(0, 3%, 20%)",
+      red: "hsl(0, 100%, 45%)",
+      lightRed: "hsl(0, 100%, 55%)",
+    }
+  }
