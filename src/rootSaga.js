094a1f87023a121840023893369df00832a7efd4
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-11-11 19:31:40 +0100

    made some adjustments in code - deleted empty lines etc.

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,5 +1,4 @@
 import { all } from "redux-saga/effects";
-
 import { tasksSaga } from "./features/tasks/tasksSaga";
 
 export default function* rootSaga() {
--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,8 @@
 export const theme = {
     colors: {
-      teal: "hsl(180, 100%, 25%)",
-      persianGreen: "hsl(180, 100%, 30%)",
-      bondiBlue: "hsl(180, 100%, 35%)",
+      teal: "hsl(180, 80%, 25%)",
+      persianGreen: "hsl(180, 80%, 30%)",
+      bondiBlue: "hsl(180, 80%, 35%)",
       white: "hsl(0, 0%, 100%)",
       mercury: "hsl(0, 0%, 90%)",
       green: "hsl(120, 93%, 30%)",
