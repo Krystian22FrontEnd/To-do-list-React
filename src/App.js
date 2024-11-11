3c61308254ac1ce72dc8c1994b88535696f68215
Krystian Krupiński <krupinskikrycha@gmail.com>
2024-11-11 19:28:57 +0100

    added react-router to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,34 @@
 import React from "react";
-import Tasks from "./features/tasks/Tasks";
-
-export default () => <Tasks/>
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import AuthorPage from "./features/author/AuthorPage"
+import { StyledNavLink, ListItem, Wrapper,Item } from "./styled";
+export default () => (
+  <HashRouter>
+    <Wrapper>
+    <Item>
+      <ListItem>
+        <StyledNavLink to="/zadania">Zadania</StyledNavLink>
+      </ListItem>
+      <ListItem>
+        <StyledNavLink to="/autor">O autorze</StyledNavLink>
+      </ListItem>
+    </Item>
+    </Wrapper>
+    <Switch>
+    <Route path="/zadania/:id">
+        <TaskPage/>
+      </Route>
+      <Route path="/zadania">
+        <TasksPage />
+      </Route>
+      <Route path="/autor">
+        <AuthorPage />
+      </Route>
+      <Route  path="/">
+        <Redirect to="/zadania" />
+      </Route>
+    </Switch>
+  </HashRouter>
+);
